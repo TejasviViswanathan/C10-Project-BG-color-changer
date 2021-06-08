262312762727b4dafb3f14d6968040f37b9fcf91
TejasviViswanathan <84506845+TejasviViswanathan@users.noreply.github.com>
2021-06-08 07:06:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,68 @@
-var btn_red;
-var btn_green;
-var r = 0;
-var g = 0;
-var b = 0;
-function setup() {
-    btn_red = createButton("click here for Red");
-    btn_green = createButton("click here for Green");
-    btn_red.position(50,450);
-    btn_green.position(175,450); 
-    btn_green.mousePressed(setGreen);
-    btn_red.mousePressed(setRed)
-    createCanvas(400, 400);
+var garden,rabbit;
+var gardenImg,rabbitImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+
 }
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+}
+
 
 function draw() {
-    background(r,g,b)
+  background(0);
+  
+  edges= createEdgeSprites();
+  rabbit.collide(edges);
+
+  rabbit.x = mouseX
+
+  console.log("Bigger " + "Square " + "is " + "Leaf")
+
+  drawSprites();
+  spawnApples();
+  spawnLeaves();
 }
 
-function setGreen(){
-    r = 0;
-    g = 255;
-    b = 0;
+function spawnApples(){
+if (frameCount%60===0){
+var apple = createSprite(random(50,350),40,10,10);
+apple.velocityY=3;
+apple.lifetime=80;
+
+}
 }
 
-function setRed(){
-    r = 255;
-    g = 0;
-    b = 0;
+function spawnLeaves(){
+  if(frameCount%60===0){
+    var leaf = createSprite(random(100,275), 40, 20, 20);
+    leaf.velocityY=4;
+    leaf.lifetime=80;
+  }
 }
 
 
+  var rand = Math.round(random(1,2));
+
+  if (frameCount % 80 == 0){
+  if (rand == 1){
+    spawnApples();
+
+    
+  }
+  else spawnLeaves();
+  }
